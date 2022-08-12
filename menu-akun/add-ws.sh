e9f04b2eb7b013200ab3d3401809d997f9823004
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-08-13 04:17:08 +0700

    Update add-ws.sh

--- menu-akun/add-ws.sh
+++ menu-akun/add-ws.sh
@@ -19,10 +19,12 @@ domain=$(cat /etc/xray/domain)
 #
 uuid9=$(cat /proc/sys/kernel/random/uuid)
 uuid=b8458948-a630-4e6d-809a-230b2223ff3d
-#
+#CONFIG DEFAULT
 urltrojan="trojan://${uuid}@${domain}:443?path=/xraytrojanws&security=tls&host=${domain}&type=ws&sni=${domain}#${user}"
+urlvless="vless://${uuid}@${domain}:443?path=/xrayws&security=tls&encryption=none&host=${domain}&type=ws&sni=${domain}#${user}"
+#CONFIG OPOK
 urltrojanopok="trojan://${uuid}@104.17.3.81:443?path=ws://api.midtrans.com/xraytrojanws&security=tls&host=${domain}&type=ws&sni=api.midtrans.com#${user}"
-urlvlessopok="vless://${uuid}@104.17.3.81}:443?path=ws://api.midtrans.com/xrayws&security=tls&encryption=none&host=${domain}&type=ws&sni=api.midtrans.com#${user}"
+urlvlessopok="vless://${uuid}@104.17.3.81:443?path=ws://api.midtrans.com/xrayws&security=tls&encryption=none&host=${domain}&type=ws&sni=api.midtrans.com#${user}"
 
 clear
 echo -e ""
@@ -53,9 +55,10 @@ echo -e "Protocol VPN: SHADOWDSOCKS"
 echo -e "Method Cipher: aes-128-gcm,aes-256-gcm,chacha20-poly1305"
 echo -e "Path: /xrayssws"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "URL CONFIG TROJAN: ${trojan:}"
+echo -e "URL CONFIG TROJAN(Default): ${urltrojan}"
+echo -e "URL CONFIG TROJAN(OPOK): ${urltrojanopok:}"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "URL CONFIG VLESS: ${vless:}"
+echo -e "URL CONFIG VLESS(OPOK): ${urlvlessopok}"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "URL CONFIG VMESS: ${vmess:}"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
